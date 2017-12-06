edc6a0d1e6a2850d74946da9b5dc29bc47a66043
Rafal Paliwoda <rp@message.pl>
2017-12-06 23:36:37 +0100

    sherlock-and-anagrams

--- src/main/groovy/strings/SherlockAndAnagrams.groovy
+++ src/main/groovy/strings/SherlockAndAnagrams.groovy
@@ -5,10 +5,33 @@ package strings
  */
 class SherlockAndAnagrams {
 
-    static def isValid(String input) {
-        false
+    static def countAnagrams(String input) {
+        def subs = []
+
+        for (int i = 0; i <= input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                subs.add(input.substring(i, j))
+            }
+        }
+
+        return subs.collect { String str -> str.toList().sort().join("") }
+                   .groupBy { it }
+                   .collect { it.value.size() }
+                   .findAll { it > 1 }
+                   .collect { it * (it - 1) / 2 }
+                   .sum()
     }
 
+
+    static void main(String... args) {
+        System.in.readLines().drop(1).each {
+            println countAnagrams(it)
+        }
+    }
 }
+
+
+
+
 
 
--- src/test/groovy/strings/SherlockAndAnagramsTest.groovy
+++ src/test/groovy/strings/SherlockAndAnagramsTest.groovy
@@ -1,9 +1,23 @@
 package strings
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SherlockAndAnagramsTest extends Specification {
 
-    // TODO
+    @Unroll
+    def "test"() {
+        expect:
+        SherlockAndAnagrams.countAnagrams(input) == expected
+
+        where:
+        input        | expected
+        "ifailuhkqq" | 3
+        "hucpoltgty" | 2
+        "ovarjsnrbf" | 2
+        "pvmupwjjjf" | 6
+        "iwwhrlkpek" | 3
+        "abba"       | 4
+    }
 
 }
